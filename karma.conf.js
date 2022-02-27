0f46861aab6d8699ad724326006e6e75f6fee29a
Hendrik Bünder <hendrik.buender@helpline.pmcs.de>
2022-02-27 12:10:27 +0100

    Test at specific folder

--- karma.conf.js
+++ karma.conf.js
@@ -42,9 +42,10 @@ module.exports = function (config) {
     // the default configuration
     junitReporter: {
       outputDir: 'test-results/karma', // results will be saved as $outputDir/$browserName.xml
-      outputFile: 'test-result', // if included, results will be saved as $outputDir/$browserName/$outputFile
+      browserName: 'Chrome',
+      outputFile: 'test-result.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
       suite: '', // suite will become the package name attribute in xml testsuite element
-      useBrowserName: true, // add browser name to report and classes names
+      useBrowserName: false, // add browser name to report and classes names
       nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
       classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
       properties: {}, // key value pair of properties to add to the <properties> section of the report
