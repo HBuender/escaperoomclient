f1bb8aed64bbaa48b7c0021c60ae50b6f0647bf4
Hendrik Bünder <hendrik.buender@helpline.pmcs.de>
2022-02-27 11:51:46 +0100

    Test Results for Sonar

--- karma.conf.js
+++ karma.conf.js
@@ -42,13 +42,13 @@ module.exports = function (config) {
     // the default configuration
     junitReporter: {
       outputDir: 'test-results/karma', // results will be saved as $outputDir/$browserName.xml
-      outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
+      outputFile: 'test-result', // if included, results will be saved as $outputDir/$browserName/$outputFile
       suite: '', // suite will become the package name attribute in xml testsuite element
       useBrowserName: true, // add browser name to report and classes names
       nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
       classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
       properties: {}, // key value pair of properties to add to the <properties> section of the report
-      xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
+      xmlVersion: '1' // use '1' if reporting to be per SonarQube 6.2 XML format
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/escaperoomclient'),
